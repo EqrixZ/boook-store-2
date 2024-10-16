3ab17c4fb4aa595efd670baf29297d8b05940be9
EqrixZ <138424366+EqrixZ@users.noreply.github.com>
2024-10-16 23:30:54 +0700

    Update SellBook.jsx

--- src/SellBook.jsx
+++ src/SellBook.jsx
@@ -12,7 +12,7 @@ const SellBook = () => {
 
   const fetchBooks = async () => {
     try {
-      const response = await axios.get('http://localhost:3000/books');
+      const response = await axios.get('https://env-5322576.proen.app.ruk-com.cloud//books');
       setBooks(response.data);
     } catch (error) {
       console.error('Error fetching books:', error);
@@ -21,7 +21,7 @@ const SellBook = () => {
 
   const sellBook = async () => {
     try {
-      await axios.delete(`http://localhost:3000/books/${selectedBookID}`);
+      await axios.delete(`https://env-5322576.proen.app.ruk-com.cloud/0/books/${selectedBookID}`);
       alert('Book sold successfully!');
       fetchBooks(); // Refresh the list
     } catch (error) {
